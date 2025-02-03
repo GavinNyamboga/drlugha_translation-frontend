734edafff4b2719dbec0bdf45146d3f216bb2f3e
Gavin Nyamboga <gavinnyamboga3@gmail.com>
2025-02-03 21:06:03 +0300

    added missed files

---
+++ src/@core/models/audio/audio-details.ts
@@ -0,0 +1,13 @@
+export interface AudioDetails {
+    voiceId: number,
+    userId: number,
+    audioLink: string,
+    fileUrl: string,
+    recordedBy: string,
+    username: string,
+    email: string,
+    gender: string,
+    accepted: boolean,
+    comment: string,
+    translatedSentenceId: number
+}
